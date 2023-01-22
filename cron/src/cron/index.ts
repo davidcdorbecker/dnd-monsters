8d3e45ec412ffa96883532e0620388cf3cf58091
davidcdorbecker <davidcdorbecker@gmail.com>
2023-01-22 12:13:01 -0600

    assets added

--- cron/src/cron/index.ts
+++ cron/src/cron/index.ts
@@ -2,4 +2,4 @@ import cron from 'node-cron'
 import {Process} from '../jobs/process_pending_transactions'
 
 cron.schedule('*/30 * * * * *', Process.runPendingTransactions);
-cron.schedule('*/10 * * * * *', Process.runInjectCredits);
+cron.schedule('0 0 */1 * * *', Process.runInjectCredits);
